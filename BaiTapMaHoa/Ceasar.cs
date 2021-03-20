7f425264fe12f9cd95367f2a32d66c3fb6b16d94
Hien Luong <hyhien@gmail.com>
2021-03-20 09:57:46 +0700

    Add project files.

---
+++ BaiTapMaHoa/Ceasar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTapMaHoa
+{
+    public class Ceasar
+    {
+        public static int MapCharacterToNumber(char ch)
+        {
+            return ch - 'A';
+        }
+
+        public static char MapNumberToCharacter(int num)
+        {
+            return (char)('A' + num);
+        }
+
+        public static int[] ConvertStringToArrayNumber(string input)
+        {
+            var result = new List<int>();
+            foreach(var ch in input)
+            {
+                result.Add(MapCharacterToNumber(ch));
+            }
+            return result.ToArray();
+        }
+    }
+}
